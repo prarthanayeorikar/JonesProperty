a26dc3d962b5306ffd7c6d18a617ea3ad49a58de
prarthanayeorikar <prarthanayeorikar7516@gmail.com>
2023-09-26 07:45:07 +0530

    jonesgroup

---
+++ src/main/java/git_test/GitTest.java
@@ -0,0 +1,14 @@
+package git_test;
+
+import java.io.IOException;
+
+import com.crm.qa.base.TestBase;
+
+public class GitTest extends TestBase {
+
+	public GitTest() throws IOException {
+		super();
+		System.out.println("new branch ");
+	}
+
+}
